786172800500ff28ebd4f0739feabd23b2284c3e
huntershen008 <huntershen008@icloud.com>
2022-09-14 23:08:05 +0800

    updated

--- app.R
+++ app.R
@@ -3,7 +3,9 @@
 # Or use the blue button on top of this file
 
 # golem default's to these options
-# pkgload::load_all(helpers = FALSE, attach_testthat = FALSE) # export_all = FALSE # ac removed: if false, IDEAFilter fails
+# pkgload::load_all(helpers = FALSE, attach_testthat = FALSE)
+# export_all = FALSE
+# ac removed: if false, IDEAFilter fails
 options( "golem.app.prod" = TRUE)
 options(shiny.sanitize.errors = FALSE)
 options(shiny.autoload.r=FALSE) # needed if remove R/_disable_autoload.R
@@ -20,11 +22,12 @@ options(timeout=9999999)
 # check()
 
 # library(githubinstall)
-install_github("huntershen008/CDISCovery", force = T)
+# install_github("huntershen008/CDISCovery", force = T)
 
 # Launch the app
 library(CDISCovery)
 run_app() # add parameters here (if any)
+
 
 # turn off any options
 # options(shiny.autoload.r=NULL) # needed if remove R/_disable_autoload.R
